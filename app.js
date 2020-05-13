426b6dbbc38349e52a113ae17b53f17fcc1ed605
Katerina Paltoglou <katpaltoglou@gmail.com>
2020-05-13 20:17:26 +0300

    remove paths to non locally-defined libraries

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ requirejs.config({
 
     paths: {
         'jquery': [
-            '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',
             './lib/jquery-2.1.3.min'
         ],
 		'pathfinding' : './lib/pathfinding/pathfinding-browser.min',
