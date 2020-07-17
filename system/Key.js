7ab8016b71688f1209cf7a442a0cefb81cac67f9
Katerina Paltoglou <katpaltoglou@gmail.com>
2020-07-17 14:26:44 +0300

    update branch with refactored code

--- system/Key.js
+++ system/Key.js
@@ -1,5 +1,3 @@
-define([], function(){
-
 var Key = {
     none: 0,
     back: 8,
@@ -48,6 +46,64 @@ var Key = {
     divide: 47
 };
 
-return Key;
-
-});
+var exported_Key_none = 0;
+var exported_Key_back = 8;
+var exported_Key_tab = 9;
+var exported_Key_enter = 13;
+var exported_Key_pause = 19;
+var exported_Key_escape = 27;
+var exported_Key_shift = 16;
+var exported_Key_space = 32;
+var exported_Key_alt = 18;
+var exported_Key_pageUp = 33;
+var exported_Key_pageDown = 34;
+var exported_Key_end = 35;
+var exported_Key_home = 36;
+var exported_Key_left = 37;
+var exported_Key_up = 38;
+var exported_Key_right = 39;
+var exported_Key_down = 40;
+var exported_Key_insert = 45;
+var exported_Key_del = 46;
+var exported_Key_d0 = 48;
+var exported_Key_d1 = 49;
+var exported_Key_d2 = 50;
+var exported_Key_d3 = 51;
+var exported_Key_d4 = 52;
+var exported_Key_d5 = 53;
+var exported_Key_d6 = 54;
+var exported_Key_d7 = 55;
+var exported_Key_d8 = 56;
+var exported_Key_d9 = 57;
+var exported_Key_A = 65;
+var exported_Key_B = 66;
+var exported_Key_C = 67;
+var exported_Key_D = 68;
+var exported_Key_E = 69;
+var exported_Key_F = 70;
+var exported_Key_G = 71;
+var exported_Key_H = 72;
+var exported_Key_I = 73;
+var exported_Key_J = 74;
+var exported_Key_K = 75;
+var exported_Key_L = 76;
+var exported_Key_M = 77;
+var exported_Key_N = 78;
+var exported_Key_O = 79;
+var exported_Key_P = 80;
+var exported_Key_Q = 81;
+var exported_Key_R = 82;
+var exported_Key_S = 83;
+var exported_Key_T = 84;
+var exported_Key_U = 85;
+var exported_Key_V = 86;
+var exported_Key_W = 87;
+var exported_Key_X = 88;
+var exported_Key_Y = 89;
+var exported_Key_Z = 90;
+var exported_Key_multiply = 42;
+var exported_Key_add = 43;
+var exported_Key_subtract = 45;
+var exported_Key_decimal = 46;
+var exported_Key_divide = 47;
+export { exported_Key_back as back, exported_Key_enter as enter, exported_Key_escape as escape, exported_Key_space as space, exported_Key_left as left, exported_Key_up as up, exported_Key_right as right, exported_Key_down as down, exported_Key_B as B, exported_Key_H as H, exported_Key_L as L, exported_Key_P as P, exported_Key_Q as Q, exported_Key_S as S };
--- system/Keyboard.js
+++ system/Keyboard.js
@@ -1,56 +1,56 @@
-define(['system/Key', 'system/ButtonState'], function(Key, ButtonState){
+import { ButtonState } from ".\\ButtonState.js";
 
-    function Keyboard_Singleton() {
-        this.keyStates = [];
-        for (var i = 0; i < 256; ++i)
-            this.keyStates.push(new ButtonState());
-        document.onkeydown = this.handleKeyDown.bind(this);
-        document.onkeyup = this.handleKeyUp.bind(this);
-    }
+function Keyboard_Singleton() {
+    this.keyStates = [];
+    for (var i = 0; i < 256; ++i)
+        this.keyStates.push(new ButtonState());
+    document.onkeydown = this.handleKeyDown.bind(this);
+    document.onkeyup = this.handleKeyUp.bind(this);
+}
 
-    Keyboard_Singleton.prototype.handleKeyDown = function (evt) {
-        var code = evt.keyCode;
-        if (code < 100) evt.preventDefault();
-        if (code < 0 || code > 255)
-            return;
-        if (!this.keyStates[code].down)
-            this.keyStates[code].pressed = true;
-        this.keyStates[code].down = true;
-    };
+Keyboard_Singleton.prototype.handleKeyDown = function (evt) {
+    var code = evt.keyCode;
+    if (code < 100) evt.preventDefault();
+    if (code < 0 || code > 255)
+        return;
+    if (!this.keyStates[code].down)
+        this.keyStates[code].pressed = true;
+    this.keyStates[code].down = true;
+};
 
-    Keyboard_Singleton.prototype.handleKeyUp = function (evt) {
-        var code = evt.keyCode;
-        if (code < 0 || code > 255)
-            return;
-        this.keyStates[code].down = false;
-    };
+Keyboard_Singleton.prototype.handleKeyUp = function (evt) {
+    var code = evt.keyCode;
+    if (code < 0 || code > 255)
+        return;
+    this.keyStates[code].down = false;
+};
 
-    Keyboard_Singleton.prototype.reset = function () {
-        for (var i = 0; i < 256; ++i)
-            this.keyStates[i].pressed = false;
-    };
+Keyboard_Singleton.prototype.reset = function () {
+    for (var i = 0; i < 256; ++i)
+        this.keyStates[i].pressed = false;
+};
 
-    Keyboard_Singleton.prototype.getA = function () {
-        for (var i = 0; i < 256; ++i)
-            {
-                if (this.keyStates[i].pressed && this.isAllowed(i)) return i;
-            }
-    };
+Keyboard_Singleton.prototype.getA = function () {
+    for (var i = 0; i < 256; ++i)
+        {
+            if (this.keyStates[i].pressed && this.isAllowed(i)) return i;
+        }
+};
 
-    Keyboard_Singleton.prototype.isAllowed = function (code) {
-        if ((code >= 48 && code <=57) || (code >= 65 && code <=90) || (code == 32))
-            return true;
-        return false;
-    };
+Keyboard_Singleton.prototype.isAllowed = function (code) {
+    if ((code >= 48 && code <=57) || (code >= 65 && code <=90) || (code == 32))
+        return true;
+    return false;
+};
 
-    Keyboard_Singleton.prototype.pressed = function (key) {
-        return this.keyStates[key].pressed;
-    };
+Keyboard_Singleton.prototype.pressed = function (key) {
+    return this.keyStates[key].pressed;
+};
 
-    Keyboard_Singleton.prototype.down = function (key) {
-        return this.keyStates[key].down;
-    };
+Keyboard_Singleton.prototype.down = function (key) {
+    return this.keyStates[key].down;
+};
 
-    var keyboard = new Keyboard_Singleton();
-	return keyboard;
-});
+var keyboard = new Keyboard_Singleton();
+var exported_keyboard = keyboard;
+export { exported_keyboard as keyboard };
