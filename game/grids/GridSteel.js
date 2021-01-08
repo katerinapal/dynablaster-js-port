9069b312426155246c79ad3d3e216aa311e67e2f
Katerina Paltoglou <katpaltoglou@gmail.com>
2021-01-08 18:17:27 +0200

    update import/export feature names

--- game/grids/GridSteel.js
+++ game/grids/GridSteel.js
@@ -1,44 +1,47 @@
-define(['game/Grid', 'game/Tile', 'system/Canvas', 'system/Sprites', 'system/Vector2', 'system/Rectangle'],
-	function(Grid, Tile, Canvas, sprites, Vector2, Rectangle){
+import { sprites } from "..\\..\\system\\Sprites.js";
+import { Rectangle } from "..\\..\\system\\Rectangle.js";
+import { Vector2 } from "..\\..\\system\\Vector2.js";
+import { Tile } from "..\\Tile.js";
+import { Grid } from "..\\Grid.js";
+import { canvas } from "..\\..\\main.js";
 
-	function GridSteel (parent, cols, rows, mode)
-	{
-		Grid.call(this, parent, cols, rows, mode);
-		this.level = 2;
-		this.type = "steel";
-	}
+function GridSteel (parent, cols, rows, mode)
+{
+    Grid.call(this, parent, cols, rows, mode);
+    this.level = 2;
+    this.type = "steel";
+}
 
-	GridSteel.prototype = Object.create(Grid.prototype);
+GridSteel.prototype = Object.create(Grid.prototype);
 
-	GridSteel.prototype.draw = function () {
-		Grid.prototype.draw.call(this);
+GridSteel.prototype.draw = function () {
+    Grid.prototype.draw.call(this);
 
-		for (var i in this.tiles)
-		{
-			var x = i % this.cols;
-			var y = Math.floor(i / this.cols);
-			var shadowPosition = this.calculateTilePosition(x,y+1);
-			canvas.drawImage(sprites.tiles.grass, shadowPosition, 0, Vector2.zero, new Rectangle(48, 0, Tile.size.width, Tile.size.height));
-		}
+    for (var i in this.tiles)
+    {
+        var x = i % this.cols;
+        var y = Math.floor(i / this.cols);
+        var shadowPosition = this.calculateTilePosition(x,y+1);
+        canvas.drawImage(sprites.tiles.grass, shadowPosition, 0, Vector2.zero, new Rectangle(48, 0, Tile.size.width, Tile.size.height));
+    }
 
-	};
+};
 
-	GridSteel.prototype.fill = function ()
-	{
+GridSteel.prototype.fill = function ()
+{
 
-		for(var i = 1; i < this.cols-1; i++)
-		{
-			for(var j = 1; j < this.rows-1; j++)
-			{
-				if (i % 2 === 0 && j % 2 === 0)
-				{
-					this.add(i,j);
-				}
-			}
-		}
+    for(var i = 1; i < this.cols-1; i++)
+    {
+        for(var j = 1; j < this.rows-1; j++)
+        {
+            if (i % 2 === 0 && j % 2 === 0)
+            {
+                this.add(i,j);
+            }
+        }
+    }
 
-	};
+};
 
-	return GridSteel;
-
-});
+var mod_GridSteel = GridSteel;
+export { mod_GridSteel as GridSteel };
--- system/Sounds.js
+++ system/Sounds.js
@@ -1,39 +1,36 @@
-define([], function(){
+function Sounds_Singleton () {
+    this.sounds = {};
+    this.soundsOn = true;
+}
 
-	function Sounds_Singleton () {
-		this.sounds = {};
-		this.soundsOn = true;
-	}
+Sounds_Singleton.prototype.soundsSet = function (state) {
+    this.soundsOn = state;
+};
 
-	Sounds_Singleton.prototype.soundsSet = function (state) {
-		this.soundsOn = state;
-	};
+Sounds_Singleton.prototype.soundsChange = function () {
+    this.soundsOn = !this.soundsOn;
+};
 
-	Sounds_Singleton.prototype.soundsChange = function () {
-		this.soundsOn = !this.soundsOn;
-	};
+Sounds_Singleton.prototype.load = function (name, vol, obj) {
+    this.sounds[name] = obj;
+    this.sounds[name].volume = vol;
+    this.sounds[name].load();
+};
 
-	Sounds_Singleton.prototype.load = function (name, vol, obj) {
-		this.sounds[name] = obj;
-		this.sounds[name].volume = vol;
-		this.sounds[name].load();
-	};
+Sounds_Singleton.prototype.play = function (name) {
+    if (this.soundsOn === true)
+        {
+            if (!this.sounds[name].ended)
+                {
+                    var ss = this.sounds[name].cloneNode();
+                    ss.volume = this.sounds[name].volume;
+                    ss.play();
+                }
+            this.sounds[name].play();
+        }
+};
 
-	Sounds_Singleton.prototype.play = function (name) {
-		if (this.soundsOn === true)
-			{
-				if (!this.sounds[name].ended)
-					{
-						var ss = this.sounds[name].cloneNode();
-						ss.volume = this.sounds[name].volume;
-						ss.play();
-					}
-				this.sounds[name].play();
-			}
-	};
+var sounds = new Sounds_Singleton();
 
-	var sounds = new Sounds_Singleton();
-
-	return sounds;
-
-});
+var mod_sounds = sounds;
+export { mod_sounds as sounds };
